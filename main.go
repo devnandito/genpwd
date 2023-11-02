7fd6e5d167b9fce5f0ff637f1dd35ae0f156d44d
Diego Saldivar <fhersa@gmail.com>
2023-11-01 21:45:23 -0300

    create func to convert pdf to img

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"image/color"
+	"image/jpeg"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
-	"github.com/fogleman/gg"
+	"github.com/jung-kurt/gofpdf"
+	"github.com/karmdip-mi/go-fitz"
 )
 
 var (
@@ -25,32 +27,25 @@ var (
 	s1 string
 	s2 string
 	gree string
-	data1 []byte
+	// data1 []byte
 	minSpecialChar = 1
 	minNum = 1
 	minUpperCase = 1
-	i = 5.0
+	// i = 5.0
 )
 
-const (
-	W = 1024
-	H = 512
-	h = 24
-)
+// const (
+// 	W = 1024
+// 	H = 512
+// 	h = 24
+// )
 
 func main() {
-	// dc := gg.NewContext(1000, 1000)
-	// dc.DrawCircle(500, 500, 400)
-	// dc.SetRGB(0, 0, 0)
-	// dc.Fill()
-	// dc.SavePNG("out.png")
-
 	for {
 		get_menu()
 		fmt.Print("Option: ")
 		fmt.Scanln(&options)
 		if options == 1 {
-			// rand.Seed(time.Now().Unix())
 			fmt.Print("Length: ")
 			fmt.Scanln(&lenPwd)
 			fmt.Print("Username: ")
@@ -62,16 +57,13 @@ func main() {
 			fmt.Print("Gretings: ")
 			fmt.Scanln(&s1,&s2)
 			gree = s1 + " " +s2
-			pwd := generatePassword(lenPwd, minSpecialChar, minNum, minUpperCase)
-			fmt.Println("Password generate success")
-			data1, lines := get_content_email(gree, system, strconv.Itoa(lenPwd), username, pwd)
-			err := os.WriteFile("./"+filename+".txt", data1, 0644)
+			pwd := GeneratePassword(lenPwd, minSpecialChar, minNum, minUpperCase)
+			data1 :=GetContentEmail(gree, system, strconv.Itoa(lenPwd), username, pwd)
+			err := os.WriteFile("./txt/"+filename+".txt", data1, 0644)
 			check(err)
-			get_draw(filename, lines)
-			// d1 := []byte("Hello\ngo\n")
-			// f, err := os.Create("./dat2")
-			// check(err)
-			// defer f.Close()
+			fmt.Println("TXT generate successfully")
+			GetPdf(filename)
+			PdfToImage(filename)
 		}
 
 		if options == 2 {
@@ -92,76 +84,34 @@ func get_menu() {
 	fmt.Println("2 Exit")
 }
 
-func get_draw(filename string, lines []string) {
-	dc := gg.NewContext(W, H)
-	dc.SetRGB(0, 0, 0)
-	//dc.LoadFontFace("/Library/Fonts/Impact.ttf", 128)
-	
-	for i, line := range lines {
-		y := H/2 - h*len(lines)/2 + i*h
-		x := float64(W/2)
-		dc.DrawStringWrapped(line, x+3, float64(y)+3 , 0.5, 0.5, W, 1.5, gg.AlignLeft)
-	}
-	
-	mask := dc.AsMask()
-
-	dc.SetRGB(1, 1, 1)
-	dc.Clear()
-
-	g := gg.NewLinearGradient(0, 0, W, H)
-	g.AddColorStop(0, color.RGBA{50, 48, 48, 255})
-	g.AddColorStop(1, color.RGBA{31, 27, 27, 255})
-	// g.AddColorStop(0, color.RGBA{255, 0, 0, 255})
-	// g.AddColorStop(1, color.RGBA{0, 0, 255, 255})
-	dc.SetFillStyle(g)
-
-	dc.SetMask(mask)
-	dc.DrawRectangle(0, 0, W, H)
-	dc.Fill()
-	dc.SavePNG(filename+".png")
-}
-
-func get_content_email(gree, system, lenPwd, username, pwd string) ([]byte, []string) {
+func GetContentEmail(gree, system, lenPwd, username, pwd string) ([]byte) {
 	var lines = []string {
 		gree,
 		fmt.Sprintf("Por este medio remito las credenciales de acceso a SIPAP %s", system),
-		"Tener en cuenta las siguientes políricas de seguridad al cambiar la contraseña:",
+		"Tener en cuenta las siguientes políticas de seguridad al cambiar la contraseña:",
 		"- No debe ser una palabra que pueda estar en algún diccionario, ni relacionado al nombre de usuario o entidad.",
 		"- No debe ser correlativo al anterior, ni similar en las últimas 10 contraseñas.",
 		fmt.Sprintf("- Debe contener por los menos una mayúscula, una minúscula, un número y un carácter especial y de longitud mínima de %s.", lenPwd),
 		"- Vigencia mínima de contraseña, de 1(un) días. Un solo cambio por día es posible.",
 		"- Escriba su contraseña en un bloc de notas para luego copiar y pegar en el sistema.",
-		"USUARIO",
+		"\nUSUARIO",
 		username,
-		"CONTRASEÑA",
+		"\nCONTRASEÑA",
 		pwd,
-		"Favor dar acuse.",
+		"\nFavor dar acuse.",
 		"Saludos cordiales.",
-
 	}
 
-	m := make(map[int]string)
-	m[1] = gree
-	m[2] = fmt.Sprintf("Por este medio remito las credenciales de acceso a SIPAP %s", system)
-	m[3] = "Tener en cuenta las siguientes políricas de seguridad al cambiar la contraseña:"
-	m[4] = "- No debe ser una palabra que pueda estar en algún diccionario, ni relacionado al nombre de usuario o entidad."
-	m[5] = "- No debe ser correlativo al anterior, ni similar en las últimas 10 contraseñas."
-	m[6] = fmt.Sprintf("- Debe contener por los menos una mayúscula, una minúscula, un número y un carácter especial y de longitud mínima de %s.", lenPwd)
-	m[7] = "- Vigencia mínima de contraseña, de 1(un) días. Un solo cambio por día es posible."
-	m[8] = "- Escriba su contraseña en un bloc de notas para luego copiar y pegar en el sistema."
-	m[9] = "\nUSUARIO"
-	m[10] = username
-	m[11] = "\nCONTRASEÑA"
-	m[12] = pwd
-	m[13] = "\nFavor dar acuse."
-	m[14] = "Saludos cordiales."
+	var txt string
+	for _, value  := range lines {
+		txt = txt + "\n" + value
+	}
+	data := []byte(string(txt))
 
-	// data1 := []byte(string(m["gree"]+"\n"+m["p1"]+"\n"+m["p2"]+"\n"+m["p3"]+"\n"+m["p4"]+"\n"+m["p5"]+"\n"+m["p6"]+"\n"+m["p7"]+"\n"+m["p8"]+"\n"+m["p9"]+"\n"+m["p10"]+"\n"+m["p11"]+"\n"+m["p12"]+"\n"+m["p13"]))
-	data1 := []byte(string(m[1]+"\n"+m[2]+"\n"+m[3]+"\n"+m[4]+"\n"+m[5]+"\n"+m[6]+"\n"+m[7]+"\n"+m[8]+"\n"+m[9]+"\n"+m[10]+"\n"+m[11]+"\n"+m[12]+"\n"+m[13]+"\n"+m[14]))
-	return data1, lines
+	return data
 }
 
-func generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase int) string {
+func GeneratePassword(passwordLength, minSpecialChar, minNum, minUpperCase int) string {
 	var password strings.Builder
 
 	//Set special character
@@ -193,5 +143,109 @@ func generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase int) 
 		inRune[i], inRune[j] = inRune[j],inRune[i]
 	})
 
+	fmt.Println("Password generate success")
 	return string(inRune)
 }
+
+func GetPdf(file string) {
+	text, err := os.ReadFile("./txt/"+file+".txt")
+	check(err)
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	// pdf.SetFont("Arial", "B", 16)
+	// pdf.MoveTo(0, 10)
+	// pdf.Cell(1, 1, "Title")
+	pdf.SetFont("Arial", "", 14)
+	pdf.MoveTo(0, 10)
+	tr := pdf.UnicodeTranslatorFromDescriptor("")
+	width, _ := pdf.GetPageSize()
+	pdf.MultiCell(width, 10, tr(string(text)), "" , "", false)
+	err = pdf.OutputFileAndClose("./pdf/"+file+".pdf")
+
+	if err == nil {
+		fmt.Println("PDF generate successfully")
+	}
+}
+
+func PdfToImage(filename string) {
+	var files []string
+	path := "./pdf/"+filename+".pdf"
+	files = append(files, path)
+
+	for _, file := range files {
+		doc, err := fitz.New(file)
+		check(err)
+		
+		for n := 0; n < doc.NumPage(); n++ {
+			img, err := doc.Image(n)
+			check(err)
+			
+			f, err := os.Create(filepath.Join("./img/",  filename+".jpg"))
+			check(err)
+
+			err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
+			check(err)
+			f.Close()
+		}
+	}
+}
+
+// func SaveToImage() {
+// 	var files []string
+// 	root := "/home/tech/go/src/genpwd/"
+// 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+// 		if filepath.Ext(path) == ".pdf" {
+// 			files = append(files, path)
+// 		}
+// 		return nil
+// 	})
+// 	check(err)
+
+// 	for _, file := range files {
+// 		doc, err := fitz.New(file)
+// 		check(err)
+// 		folder :=strings.TrimSuffix(path.Base(file), filepath.Ext(path.Base(file)))
+
+// 		for n := 0; n < doc.NumPage(); n++ {
+// 			img, err := doc.Image(n)
+// 			check(err)
+// 			err = os.MkdirAll("img/"+folder, 0755)
+// 			check(err)
+// 			f, err := os.Create(filepath.Join("img/"+folder+"/", fmt.Sprintf("image-%05d.jpg", n)))
+// 			check(err)
+// 			err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
+// 			check(err)
+// 			f.Close()
+// 		}
+// 	}
+// }
+
+// func GetDraw(filename string, lines []string) {
+// 	dc := gg.NewContext(W, H)
+// 	dc.SetRGB(0, 0, 0)
+// 	//dc.LoadFontFace("/Library/Fonts/Impact.ttf", 128)
+	
+// 	for i, line := range lines {
+// 		y := H/2 - h*len(lines)/2 + i*h
+// 		x := float64(W/2)
+// 		dc.DrawStringWrapped(line, x+3, float64(y)+3 , 0.5, 0.5, W, 1.5, gg.AlignLeft)
+// 		// dc.DrawString(line, x+3, float64(y)+3)
+// 	}
+	
+// 	mask := dc.AsMask()
+
+// 	dc.SetRGB(1, 1, 1)
+// 	dc.Clear()
+
+// 	g := gg.NewLinearGradient(0, 0, W, H)
+// 	g.AddColorStop(0, color.RGBA{50, 48, 48, 255})
+// 	g.AddColorStop(1, color.RGBA{31, 27, 27, 255})
+// 	// g.AddColorStop(0, color.RGBA{255, 0, 0, 255})
+// 	// g.AddColorStop(1, color.RGBA{0, 0, 255, 255})
+// 	dc.SetFillStyle(g)
+
+// 	dc.SetMask(mask)
+// 	dc.DrawRectangle(0, 0, W, H)
+// 	dc.Fill()
+// 	dc.SavePNG(filename+".png")
+// }
